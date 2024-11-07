032d838fb5813e8740ccfb4369681b67ad51bd0c
MrAnas-Dot <smohamedanas99@gmail.com>
2024-11-07 16:19:28 +0530

    craeted a html and css files i studies in besant

---
+++ love.py
@@ -0,0 +1,10 @@
+#love_note = "i love you chellammaa ummmaaaaaaaaa"
+#count = 0
+#for love in range(100):
+#   count += 1
+#  print(f"{count}.{love_note}")
+  
+  #whatsapp automation
+  
+  #import pywhatkit
+  #pywhatkit.sendwhatmsg('+918098520793','Hi',16,29)
